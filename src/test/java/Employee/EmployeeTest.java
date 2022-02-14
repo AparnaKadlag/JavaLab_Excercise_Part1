fd24e965a410f9bcbf7f85511a9c8bc844638e49
Aparna <aparnajagtap41@gmail.com>
2022-02-14 18:38:31 +0530

    first commit

---
+++ src/test/java/Employee/EmployeeTest.java
@@ -0,0 +1,25 @@
+package Employee;
+
+import static org.junit.Assert.*;
+public class EmployeeTest {
+
+    @org.junit.Test
+    public void getInfo() {
+    }
+
+    @org.junit.Test
+    public void computeBaseSalary() {
+    }
+
+    @org.junit.Test
+    public void addSal() {
+    }
+
+    @org.junit.Test
+    public void addWork() {
+    }
+
+    @org.junit.Test
+    public void getSalary() {
+    }
+}
