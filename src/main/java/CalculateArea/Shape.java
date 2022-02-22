f59ddd8769bab84dc546f703ead2195729ce597c
Aparna Kadlag <aparnajagtap41@gmail.com>
2022-02-22 19:05:38 +0530

    changes updated

--- src/main/java/CalculateArea/Shape.java
+++ src/main/java/CalculateArea/Shape.java
@@ -10,8 +10,8 @@ public class Shape {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter Radius = ");
         double radius1=sc.nextDouble();
-        Circle c= new Circle(radius1);
-        c.showResult();
+        Circle circle= new Circle(radius1);
+        circle.showResult();
 
         //User input of parameter for Rectangle
         System.out.println("Enter length of Rectangle = ");
@@ -19,14 +19,14 @@ public class Shape {
 
         System.out.println("Enter length of Rectangle = ");
         int width =sc.nextInt();
-        Rectangle r = new Rectangle(length, width);
-        r.showResult();
+        Rectangle rectangle = new Rectangle(length, width);
+        rectangle.showResult();
 
         //User Input for Square
         System.out.println("Enter side of sqaure = ");
         double side =sc.nextDouble();
-        Square s = new Square(side);
-        s.showResult();
+        Square square = new Square(side);
+        square.showResult();
     }
 
 }
